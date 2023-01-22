ec3b5eabd8721cc0131d2295c95b7d641d55369e
Abhijit Kumar <abhijitkumar@Abhijits-MacBook-Air.local>
2023-01-22 20:04:22 +0530

    build react

--- backend/database/config.js
+++ backend/database/config.js
@@ -3,10 +3,14 @@ const colors = require('colors')
 const url = "mongodb://localhost:27017"
 // const database_name = 'linkzifyv2'
 mongoose.set('strictQuery', true);
-
+let connectionString = url+ "/" + process.env.DB_NAME
+let altlas_url = "mongodb+srv://linkzify_v2:DB%40abhikr51@cluster0.qgtchhz.mongodb.net/linkzify_v2"
+if(altlas_url){
+    connectionString = altlas_url
+}
 //via mongoose package
 const __db_connect = async ()=>{
-    await mongoose.connect(url+ "/" + process.env.DB_NAME)
+    await mongoose.connect(connectionString)
     .then(() => console.log('============ Connected! MongoDB with Mongoose ============'.magenta));
 
 }
